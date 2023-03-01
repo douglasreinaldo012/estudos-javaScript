8dde6f4c4431c5f4ba6db7f89be992e20797afea
Douglas Reinaldo <123218252+douglasreinaldo012@users.noreply.github.com>
2023-03-01 14:52:59 -0300

    Create ambiente.js

---
+++ repeticoes/ambiente.js
@@ -0,0 +1,17 @@
+var cont = 1
+console.log('::::::::teste while:::::::::::::::')
+while(cont<=3){
+    console.log(`passo ${cont}`)
+    cont = cont+1
+}
+console.log('::::::::teste Do While::::::::::::')
+var cont2 = 0
+do{
+    console.log(`passo ${cont2}`)
+    cont2++
+}while(cont2<=5)
+console.log('::::::::teste For::::::::::::::::::')
+var cont3 = 0
+for(cont3 = 0;cont3<4;cont3++){
+    console.log(`passo ${cont3}`)
+}
